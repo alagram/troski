d67b2c7b8a03e5790f97d744114ce45592a8499a
Albert Agram <albert.agram@gmail.com>
2014-03-24 14:38:03 +0000

    Generate the Rails 4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trotromate::Application.config.secret_key_base = 'f2a6c10cf74c7c20df35983e53fb3e1cf832562716e17e372ebd9a071961a732870877087c8c7c10afac366a75f0816dd8dd4927341de1b7d8dd86a1bc981f47'
