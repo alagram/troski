8a86d1960c87a4935ddaf47d77ffe3ac68c07726
Albert Agram <albert.agram@gmail.com>
2014-03-28 20:28:09 +0000

    Add permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,14 @@ Trotromate::Application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy', as: 'signout'
 
   namespace :admin do
     root to: 'base#index'
-    resources :users
+    resources :users do
+      resources :permissions
+
+      put 'permissions', to: 'permissions#set', as: 'set_permissions'
+    end
   end
 end
