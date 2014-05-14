77223a0a345c15620e13b1174b86d0c1efeb2ad3
Albert Agram <albert.agram@gmail.com>
2014-05-14 13:57:06 +0000

    Add label-based searching for tags.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -15,6 +15,26 @@ class Ticket < ActiveRecord::Base
   attr_accessor :tag_names
   before_create :associate_tags
 
+  def self.search(query)
+    query
+      .split(" ")
+      .collect do |query|
+        query.split(":")
+      end.inject(self) do |klass, (name, q)|
+        plural_name = name.pluralize.to_sym
+        association = klass.reflect_on_association(plural_name)
+        association_table = association.klass.arel_table
+
+        if [:has_and_belongs_to_many,
+            :belongs_to].include?(association.macro)
+          joins(name.pluralize.to_sym)
+            .where(association_table["name"].eq(q))
+        else
+          all
+        end
+      end
+  end
+
   private
 
   def associate_tags
