4b727208db60566e0d4a74842406a6813048bfc5
Albert Agram <albert.agram@gmail.com>
2014-05-15 20:51:07 +0000

    add CommentObserver class

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Trotromate
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.enforce_available_locales = true
     # config.i18n.default_locale = :de
+    config.active_record.observers = :comment_observer
   end
 end
